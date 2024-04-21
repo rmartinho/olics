c8d43a1161fa0d2fcc67cc53295d9e86714e5f02
R. Martinho Fernandes <rmf@rmf.io>
2024-04-21 19:40:00 +0200

    Allow use of overrides

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import ical from 'ical-generator'
 import { DateTime } from 'luxon'
+
 import parse from './rss-to-json'
 
 export default {
-  async fetch(_request: Request, _env: Env, _ctx: ExecutionContext): Promise<Response> {
-    const content = await makeIcs()
+  async fetch(_request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
+    const content = await makeIcs(env)
     return new Response(content, {
       headers: {
+        'Cache-Control': 'public, max-age=3600, s-max-age=3600',
         'Content-Type': 'text/calendar',
         'Content-Disposition': 'attachment; filename=otherland.ics',
       },
@@ -46,14 +48,15 @@ type RssItem = {
   link: string
 }
 
-async function makeIcs() {
+async function makeIcs(env: Env) {
   const rss = await parse(feedUrl)
 
   const calendar = ical({ name: 'Otherland Events' })
   calendar.timezone(EuropeBerlinTz)
-  const pattern = /(\d+). ([A-Z][a-zä][a-z]) (\d{4}) (\((\d+):(\d+)\))?(.*)/
-  rss!.items.forEach((it: RssItem) => {
-    const match = it.title.match(pattern)
+  const pattern = /(\d+). ([A-Z][a-zä][a-z]) (\d{4}) \((\d+):(\d+)\) (.*)/
+  for (const it of rss!.items as RssItem[]) {
+    const title = await override(env, it.title)
+    const match = title.match(pattern)
     if (!match) throw `bad match ${it.title}`
     const year = Number(match[3])
     const month = {
@@ -72,10 +75,10 @@ async function makeIcs() {
     }[match[2]]
     if (month == null) throw `bad month ${it.title}`
     const day = Number(match[1])
-    const hour = Number(match[5] ?? 0)
-    const minute = Number(match[6] ?? 0)
+    const hour = Number(match[4])
+    const minute = Number(match[5])
     const start = DateTime.fromObject({ year, month, day, hour, minute }, { zone: 'Europe/Berlin' })
-    const summary = match[7].trim()
+    const summary = match[6].trim()
     calendar.createEvent({
       start,
       summary,
@@ -83,6 +86,11 @@ async function makeIcs() {
       url: it.link,
       timezone: 'Europe/Berlin',
     })
-  })
+  }
   return calendar.toString()
+}
+
+async function override(env: Env, title: string): Promise<string> {
+  const overridden = await env.olics.get(title)
+  return overridden ?? title
 }
