b25879e1d0529787d7f7c59457c0dc7e1c03479b
R. Martinho Fernandes <rmf@rmf.io>
2024-04-21 17:59:10 +0200

    Convert olics code to CF worker

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,64 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import ical from 'ical-generator'
+import { DateTime } from 'luxon'
+import parse from 'rss-to-json'
+
+type RssItem = {
+  title: string
+  description: string
+  link: string
+}
+
+const feedUrl = 'https://www.otherland-berlin.de/share/otherland-events.xml'
+
+import { getVtimezoneComponent } from '@touch4it/ical-timezones'
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const content = await makeIcs()
+    return new Response(content, { headers: { 'Content-Type': 'text/calendar' } })
+  },
+}
+
+async function makeIcs() {
+  const rss = await parse(feedUrl)
+
+  const calendar = ical({ name: 'Otherland Events' })
+  calendar.timezone({
+    name: 'Europe/Berlin',
+    generator: getVtimezoneComponent,
+  })
+  const pattern = /(\d+). ([A-Z][a-zä][a-z]) (\d{4}) \((\d+):(\d+)\) (.*)/
+  rss.items.forEach((it: RssItem) => {
+    const match = it.title.match(pattern)
+    if (!match) throw `bad match ${it.title}`
+    const year = Number(match[3])
+    const month = {
+      Jan: 1,
+      Feb: 2,
+      Mär: 3,
+      Apr: 4,
+      Mai: 5,
+      Jun: 6,
+      Jul: 7,
+      Aug: 8,
+      Sep: 9,
+      Okt: 10,
+      Nov: 11,
+      Dez: 12,
+    }[match[2]]
+    if (month == null) throw `bad month ${it.title}`
+    const day = Number(match[1])
+    const hour = Number(match[4])
+    const minute = Number(match[5])
+    const start = DateTime.fromObject({ year, month, day, hour, minute }, { zone: 'Europe/Berlin' })
+    const summary = match[6]
+    calendar.createEvent({
+      start,
+      summary,
+      description: it.description,
+      url: it.link,
+      timezone: 'Europe/Berlin',
+    })
+  })
+  return calendar.toString()
+}
