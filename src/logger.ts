873d8c987a8ecb0e03071e3df83741fefbf79265
R. Martinho Fernandes <rmf@rmf.io>
2024-05-02 14:34:48 +0200

    Fix use CRLF for mail

--- src/logger.ts
+++ src/logger.ts
@@ -12,14 +12,14 @@ log.init = (env: Env) => {
   options = { env }
 }
 
-log.send = async () => {
+log.send = () => {
   const msg = createMimeMessage()
   msg.setSender({ name: 'olics', addr: 'logs@pinus.rmf.io' })
   msg.setRecipient('olics@rmf.io')
   msg.setSubject('Execution logs from olics')
   msg.addMessage({
     contentType: 'text/plain',
-    data: logs.join('\n'),
+    data: logs.join('\r\n'),
   })
 
   var message = new EmailMessage('logs@pinus.rmf.io', 'olics@rmf.io', msg.asRaw())
