dbb7c531ebb24032389cfd3c704e31bacd43be7b
R. Martinho Fernandes <rmf@rmf.io>
2024-05-02 14:40:10 +0200

    Do not send empty emails

--- src/logger.ts
+++ src/logger.ts
@@ -5,22 +5,20 @@ import { createMimeMessage } from 'mimetext/browser'
 async function log(message: string) {
   const timestamp = DateTime.now().toISO()
   message = `${timestamp} ${message}`
-  logs.push(message)
+  logs.push(message.replace('\n', '\r\n'))
   console.log(message)
 }
 log.init = (env: Env) => {
   options = { env }
 }
 
-log.send = () => {
+log.send = async () => {
+  if (!logs.length) return
   const msg = createMimeMessage()
   msg.setSender({ name: 'olics', addr: 'logs@pinus.rmf.io' })
   msg.setRecipient('olics@rmf.io')
   msg.setSubject('Execution logs from olics')
-  msg.addMessage({
-    contentType: 'text/plain',
-    data: logs.join('\r\n'),
-  })
+  msg.addMessage({ contentType: 'text/plain', data: logs.join('\r\n') })
 
   var message = new EmailMessage('logs@pinus.rmf.io', 'olics@rmf.io', msg.asRaw())
   return options.env.email.send(message)
